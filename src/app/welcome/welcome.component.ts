ce897ab2ec4a03855a81d3a6b879a38168e1f0d0
Daniel <dgrimes@levelsetsolutions.com>
2018-03-06 13:39:34 -0500

    added nested component, image that's still unused

---
+++ src/app/welcome/welcome.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'bs-welcome',
+    templateUrl: './welcome.component.html'
+})
+
+export class WelcomeComponent {
+    title = "IT'S A WELCOME, LADS";
+}
