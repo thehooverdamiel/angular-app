227b28841be2a02dee0707a10a76735e3dd9278d
Daniel <dgrimes@levelsetsolutions.com>
2018-03-06 13:18:39 -0500

    some tweaks while we get into components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'bs-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = "We're making a book store";
 }
